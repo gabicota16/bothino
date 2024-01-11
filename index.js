71e8a70af65684f5bf268235dd0931e4025dcfa0
gabicota16 <134754166+gabicota16@users.noreply.github.com>
2024-01-11 15:42:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Botruco', {
+say('Bothino', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
